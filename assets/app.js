93fe50d1da7e899e4fa4f15aae55d826357dcf58
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-03-13 13:38:16 -0500

    basic pseudo code for our javascript

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,36 @@
 // Derek's API key: PAisRBGA4QirbOgNlXSpsdLZaAHQit2F
+
+// Article search:  /articlesearch.json?q={query}&fq={filter} 
+// Example call: https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
+
+//user willl enter search name 
+
+//when clicked on the search button 
+
+    //set a variable to name inputted 
+
+    //set a varible to be the url 
+    //queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?
+    //q=" + var for name to be inputted + "&api-key=PAisRBGA4QirbOgNlXSpsdLZaAHQit2F"
+
+    //call ajax in the jquery
+
+    //then do a function based on the response 
+
+    //log the response 
+
+    //make a for loop to go through the articles that pulled up in the search option 
+
+    //make a new div for each article 
+
+    //make a header or something for the title 
+
+    //set the text to be the title 
+
+    //append to the made div variable 
+
+    //append to the  html div designated for the inforamtion 
+
+    
+
+
